f3dcf6578cf40990d90a3aa4742cb0c3bde80c21
sitasreturn <87218882+sitasreturn@users.noreply.github.com>
2022-07-14 14:36:40 +0700

    wrong folder sorry

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -1,8 +1,83 @@
 // priority: 0
 
-console.info('Hello, World! (You will see this line every time client resources reload)')
+console.info('gagag)')
 
+// jei hidden items
 onEvent('jei.hide.items', event => {
-	// Hide items in JEI here
-	// event.hide('minecraft:cobblestone')
+	
+	// Immersive Engineering
+	
+	event.hide('immersiveengineering:ingot_silver');
+    event.hide('immersiveengineering:ingot_lead');
+	event.hide('immersiveengineering:ingot_electrum');
+	event.hide('immersiveengineering:ingot_constantan');
+	event.hide('immersiveengineering:ingot_uranium');
+	event.hide('immersiveengineering:ingot_nickel');
+	event.hide('immersiveengineering:nugget_silver')
+    event.hide('immersiveengineering:nugget_lead')
+    event.hide('immersiveengineering:nugget_electrum')
+    event.hide('immersiveengineering:nugget_constantan')
+    event.hide('immersiveengineering:nugget_uranium')
+    event.hide('immersiveengineering:nugget_nickel')
+	event.hide('immersiveengineering:ore_lead');
+	event.hide('immersiveengineering:ore_uranium');
+	event.hide('immersiveengineering:ore_silver');
+	event.hide('immersiveengineering:ore_nickel');
+	event.hide('immersiveengineering:deepslate_ore_lead');
+	event.hide('immersiveengineering:deepslate_ore_uranium');
+	event.hide('immersiveengineering:deepslate_ore_silver');
+	event.hide('immersiveengineering:deepslate_ore_nickel');
+	event.hide('immersiveengineering:raw_silver');
+	event.hide('immersiveengineering:raw_nickel');
+	event.hide('immersiveengineering:raw_uranium');
+	event.hide('immersiveengineering:raw_lead');
+	event.hide('immersiveengineering:raw_block_lead');
+	event.hide('immersiveengineering:raw_block_silver');
+	event.hide('immersiveengineering:raw_block_nickel');
+	event.hide('immersiveengineering:raw_block_uranium');
+    event.hide('immersiveengineering:dust_copper');
+	event.hide('immersiveengineering:dust_iron');
+	event.hide('immersiveengineering:dust_gold');
+	event.hide('immersiveengineering:dust_lead');
+	event.hide('immersiveengineering:dust_silver');
+	event.hide('immersiveengineering:dust_nickel');
+	event.hide('immersiveengineering:dust_uranium');
+	event.hide('immersiveengineering:dust_constantan');
+	event.hide('immersiveengineering:dust_electrum');
+	event.hide('immersiveengineering:dust_steel')
+	event.hide('immersiveengineering:dust_wood');
+	event.hide('immersiveengineering:dust_sulfur');
+	event.hide('immersiveengineering:dust_saltpeter');
+
+	//Mekanism
+	
+	event.hide('mekanism:tin_ingot');
+	event.hide('mekanism:lead_ingot');
+	event.hide('mekanism:bronze_ingot')
+    event.hide('mekanism:nugget_tin')
+    event.hide('mekanism:nugget_lead') 
+    event.hide('mekanism:nugget_bronze')
+	event.hide('mekanism:tin_ore');
+	event.hide('mekanism:lead_ore');
+	event.hide('mekanism:raw_tin');
+	event.hide('mekanism raw_lead');
+	event.hide('mekanism:block_raw_tin');
+	event.hide('mekanism:block_raw_lead');
+	event.hide('mekanism:sawdust');
+	event.hide('mekanism:dust_bronze');
+	event.hide('mekanism:dust_lapis_lazuli');
+	event.hide('mekanism:dust_quartz');
+	event.hide('mekanism:dust_diamond');
+	event.hide('mekanism:dust_emerald');
+	event.hide('mekanism:dust_netherite');
+	event.hide('mekanism:dust_sulfur');
+	event.hide('mekanism:dust_iron');
+	event.hide('mekanism:dust_gold');
+	event.hide('mekanism:dust_copper');
+	event.hide('mekanism:dust_tin');
+	event.hide('mekanism:dust_lead');
+    
+	//Lazier AE2
+	
+	event.hide('lazierae2:coal_dust');
 })
